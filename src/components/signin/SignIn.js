0cd7342b57c636c90331f3f8499fd427811964b4
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-17 21:09:12 +0100

    added materializecss

---
+++ src/components/signin/SignIn.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './SignIn.css';
+
+class SignIn extends Component{
+    render(){
+        return(
+            <h1>SignIn</h1>
+        );
+    }
+}
+
+export default SignIn;
