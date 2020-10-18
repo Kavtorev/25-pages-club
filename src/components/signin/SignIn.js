654aa3013ab9a2470bf1d1c45650ab9105e09ed8
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-18 17:32:00 +0100

    signin page

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,12 +1,63 @@
-import React, {Component} from 'react';
-import './SignIn.css';
+import React, { Component } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.js";
+import "./SignIn.css";
+import reader from "./undraw_book_lover_mkck.png";
+import { Link } from "react-router-dom";
 
-class SignIn extends Component{
-    render(){
-        return(
-            <h1>SignIn</h1>
-        );
-    }
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="flexHelper">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m12 l6">
+              <img className="decImage" src={reader} alt="" />
+            </div>
+            <div className="col s12 m12 l6">
+              <div className="row">
+                <div className="col s12">
+                  <h3 className="center-align">Sign In</h3>
+                </div>
+              </div>
+              <div className="row">
+                <div class="input-field col s12 m12 l10">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="icon_prefix" type="text" class="validate" />
+                  <label htmlFor="icon_prefix">Username / Email</label>
+                </div>
+              </div>
+              <div className="row">
+                <div class="input-field col s12 m12 l10">
+                  <i class="material-icons prefix">lock_outline</i>
+                  <input
+                    id="password"
+                    type="password"
+                    class="validate"
+                    autoComplete="off"
+                  />
+                  <label htmlFor="password">Password</label>
+                  <p className="resetPassword">Forgot password?</p>
+                </div>
+              </div>
+              <div className="row center-align">
+                <a href="google.com" class="waves-effect waves-light btn">
+                  Login
+                </a>
+              </div>
+              <div className="row center-align">
+                {/* <p className="register">Register</p> */}
+                <Link to="/register" className="alternative">
+                  {" "}
+                  Register{" "}
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
