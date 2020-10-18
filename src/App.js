294ef9a1e592717819c7cdd96326bd26ca8da6cb
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-18 17:29:58 +0100

    integrated react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import Register from './components/register/Register.js';
+import React from "react";
+import "./App.css";
+import Register from "./components/register/Register";
+import { Switch, Route } from "react-router-dom";
+import SignIn from "./components/signin/SignIn.js";
 
-function App(){
-  return(
-    <Register />
+function App() {
+  return (
+    <Switch>
+      <Route path="/register" component={Register} />
+      <Route path="/signin" component={SignIn} />
+    </Switch>
   );
 }
 
