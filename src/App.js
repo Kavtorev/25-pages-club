caf90051d96e65e147a9a5c56c173011b9020ac4
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-19 17:03:09 +0100

    home page component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-import Register from "./components/register/Register";
+import Register from "./components/register/Register.js";
 import { Switch, Route } from "react-router-dom";
 import SignIn from "./components/signin/SignIn.js";
+import Home from "./components/home/Home.js";
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/" component={Home} />
       <Route path="/register" component={Register} />
       <Route path="/signin" component={SignIn} />
     </Switch>
