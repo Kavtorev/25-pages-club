0df68ae6bd19e4aa76625d4f532ffb714ff5de27
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-20 18:25:58 +0100

    JSX names fix

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -6,6 +6,22 @@ import reader from "./undraw_book_lover_mkck.png";
 import { Link } from "react-router-dom";
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usernameEmail: "",
+      password: "",
+    };
+  }
+  // using ES6 arrow functions in order to avoid context binding.
+  handleUsernameEmailChange = (event) => {
+    this.setState({ usernameEmail: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
   render() {
     return (
       <div className="flexHelper">
@@ -21,32 +37,37 @@ class SignIn extends Component {
                 </div>
               </div>
               <div className="row">
-                <div class="input-field col s12 m12 l10">
-                  <i class="material-icons prefix">account_circle</i>
-                  <input id="icon_prefix" type="text" class="validate" />
+                <div className="input-field col s12 m12 l10">
+                  <i className="material-icons prefix">account_circle</i>
+                  <input
+                    id="icon_prefix"
+                    type="text"
+                    className="validate"
+                    onChange={this.handleUsernameEmailChange}
+                  />
                   <label htmlFor="icon_prefix">Username / Email</label>
                 </div>
               </div>
               <div className="row">
-                <div class="input-field col s12 m12 l10">
-                  <i class="material-icons prefix">lock_outline</i>
+                <div className="input-field col s12 m12 l10">
+                  <i className="material-icons prefix">lock_outline</i>
                   <input
                     id="password"
                     type="password"
-                    class="validate"
+                    className="validate"
                     autoComplete="off"
+                    onChange={this.handlePasswordChange}
                   />
                   <label htmlFor="password">Password</label>
                   <p className="resetPassword">Forgot password?</p>
                 </div>
               </div>
               <div className="row center-align">
-                <a href="google.com" class="waves-effect waves-light btn">
+                <a href="google.com" className="waves-effect waves-light btn">
                   Login
                 </a>
               </div>
               <div className="row center-align">
-                {/* <p className="register">Register</p> */}
                 <Link to="/register" className="alternative">
                   {" "}
                   Register{" "}
