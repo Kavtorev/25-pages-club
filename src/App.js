7f5302ee9f9ce5c594844677dfa76a8902ace989
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-23 16:47:59 +0100

    updated route structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import Register from "./components/register/Register.js";
 import { Switch, Route } from "react-router-dom";
 import SignIn from "./components/signin/SignIn.js";
-import Home from "./components/home/Home.js";
+import NavigationBar from "./components/navigationbar/NavigationBar";
+import WorkShopContainer from "./containers/workshopcontainer/WorkShopContainer";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/register" component={Register} />
-      <Route path="/signin" component={SignIn} />
-    </Switch>
+    <Fragment>
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route path="/workshop">
+          <NavigationBar />
+          <WorkShopContainer />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
