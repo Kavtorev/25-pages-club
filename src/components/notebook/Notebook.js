a0a541d14adbbc16a335f2344264fd57d5bdffd0
Dima Kavtorev <m305907@alunos.ipb.pt>
2020-10-25 22:16:53 +0000

    basic state management for 'notebook' component

--- src/components/notebook/Notebook.js
+++ src/components/notebook/Notebook.js
@@ -1,92 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Notebook.css";
 import M from "materialize-css";
 import "materialize-css/extras/noUiSlider/nouislider.css";
 import noUiSlider from "materialize-css/extras/noUiSlider/nouislider";
+import { useHistory } from "react-router-dom";
+import Carousel from "../carousel/Carousel";
 
-class Notebook extends React.Component {
-  componentDidMount() {
-    this.initializeCarousel();
-    this.initializeRange();
-  }
+function Notebook() {
+  const [note, setNote] = useState("I will note it later");
+  const [range, setRange] = useState({
+    start: 0,
+    end: 0,
+  });
 
-  initializeCarousel() {
-    const elems = document.querySelectorAll(".carousel");
-    M.Carousel.init(elems, {});
-  }
+  // returns a history object, which allows to force redirection (temp)
+  const history = useHistory();
 
-  initializeRange() {
-    var slider = document.getElementById("slider");
+  useEffect(() => {
+    const slider = initializeRange();
+    bindEventListenerSlider(slider);
+  }, []);
+
+  const initializeRange = () => {
+    const slider = document.getElementById("slider");
     noUiSlider.create(slider, {
-      start: [20, 80],
+      start: [0, 80],
+      step: 1,
       connect: true,
       range: {
         min: 0,
         max: 100,
       },
+      format: {
+        from: function (value) {
+          return parseInt(value);
+        },
+        to: function (value) {
+          return parseInt(value);
+        },
+      },
     });
-  }
 
-  render() {
-    return (
-      <div id="notebook" className="notebook__wrapper">
-        <div className="row center-align">
-          <p className="book__title truncate">
-            Sapiens. A brief History of Humankind.
-          </p>
-        </div>
-        <div className="row">
-          <div className="carousel__wrapper">
-            <div className="carousel">
-              <a className="carousel-item" href="#one!">
-                <img
-                  src="https://i.insider.com/558033ac6bb3f76a7b3304f4?width=1100&format=jpeg&auto=webp"
-                  alt=""
-                />
-              </a>
-              <a className="carousel-item" href="#one!">
-                <img
-                  src="https://i.insider.com/558033ac6bb3f76a7b3304f4?width=1100&format=jpeg&auto=webp"
-                  alt=""
-                />
-              </a>
-              <a className="carousel-item" href="#one!">
-                <img
-                  src="https://i.insider.com/558033ac6bb3f76a7b3304f4?width=1100&format=jpeg&auto=webp"
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="slider__margin">
-          <div className="row">
-            <div className="col s12 m10 offset-m1 l8 offset-l2">
-              <p className="note__hint">Range of pages</p>
-              <div id="slider"></div>
-            </div>
-          </div>
-        </div>
+    // referring to the documentation, this is one of the possible ways to bind an event listener
+    return slider;
+  };
+
+  const bindEventListenerSlider = (slider) => {
+    slider.noUiSlider.on("update", (values) => {
+      setRange((prevState) => {
+        const rangeObject = {
+          start: values[0],
+          end: values[1],
+        };
+        return { ...prevState, ...rangeObject };
+      });
+    });
+  };
+
+  const handleOnChangeTextArea = (event) => {
+    if (!event.target.value.trim()) {
+      setNote("I will note it later.");
+    } else {
+      setNote(event.target.value);
+    }
+  };
+
+  const handleOnClickButton = () => {
+    history.push("/workshop");
+    return null;
+  };
+
+  return (
+    <div id="notebook" className="notebook__wrapper">
+      <Carousel />
+      <div className="slider__margin">
         <div className="row">
           <div className="col s12 m10 offset-m1 l8 offset-l2">
-            <div className="input-field">
-              <textarea
-                id="textarea2"
-                className="materialize-textarea"
-                data-length="120"
-              ></textarea>
-              <label htmlFor="textarea2">Your thoughts</label>
-            </div>
+            <p className="note__hint">Range of pages</p>
+            <div id="slider"></div>
           </div>
         </div>
-        <div className="row center-align">
-          <a href="#!" className="waves-effect waves-light btn">
-            <i className="material-icons left">cloud</i>Save
-          </a>
+      </div>
+      <div className="row">
+        <div className="col s12 m10 offset-m1 l8 offset-l2">
+          <div className="input-field">
+            <textarea
+              id="textarea2"
+              className="materialize-textarea"
+              data-length="120"
+              onChange={handleOnChangeTextArea}
+            ></textarea>
+            <label htmlFor="textarea2">Your thoughts</label>
+          </div>
         </div>
       </div>
-    );
-  }
+      <div className="row center-align">
+        <a
+          href="#!"
+          className="waves-effect waves-light btn"
+          onClick={handleOnClickButton}
+        >
+          <i className="material-icons left">cloud</i>Save
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Notebook;
