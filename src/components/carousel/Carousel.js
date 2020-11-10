86bb53f7eee26750e50eac210d9a202184f773c4
Dima Kavtorev <dmitrijkavtorev@gmail.com>
2020-11-10 17:15:28 +0000

    feature: book title dynamically change

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,40 +1,51 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import M from "materialize-css";
 import "./Carousel.css";
 
 function Carousel() {
+  const [title, setTitle] = useState("");
+
   useEffect(() => {
     initializeCarousel();
   }, []);
 
   const initializeCarousel = () => {
     const elems = document.querySelectorAll(".carousel");
-    M.Carousel.init(elems, {});
+    const options = {
+      dist: -10,
+      // a great way to track current book
+      onCycleTo: function (event) {
+        const name = event.getAttribute("name");
+        setTitle(name);
+      },
+    };
+    M.Carousel.init(elems, options);
   };
+
   return (
     <Fragment>
       <div className="row center-align booktitle__wrapper">
-        <p className="book__title truncate">
-          Sapiens. A brief History of Humankind.
-        </p>
+        <p className="book__title truncate">{title}</p>
       </div>
       <div className="row">
         <div className="carousel">
-          <a className="carousel-item" href="#one!">
+          <a
+            className="carousel-item"
+            href="#one!"
+            name="Homo Sapiens a brief history of HumanKind"
+          >
             <img
               src="https://i.insider.com/558033ac6bb3f76a7b3304f4?width=1100&format=jpeg&auto=webp"
               alt=""
             />
           </a>
-          <a className="carousel-item" href="#one!">
+          <a
+            className="carousel-item"
+            href="#one!"
+            name="Homo Deus a brief history of Future"
+          >
             <img
-              src="https://images-na.ssl-images-amazon.com/images/I/714Pp4S+nWL.jpg"
-              alt=""
-            />
-          </a>
-          <a className="carousel-item" href="#one!">
-            <img
-              src="https://m.media-amazon.com/images/I/41k+WVPLwZL.jpg"
+              src="https://i.insider.com/558033ac6bb3f76a7b3304f4?width=1100&format=jpeg&auto=webp"
               alt=""
             />
           </a>
