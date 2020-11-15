3cfd070c917e0dec668b8d96f509e4d3cf5d49d8
Dima Kavtorev <dmitrijkavtorev@gmail.com>
2020-11-15 18:52:36 +0000

    rewritten notebook

--- src/components/notebook/Notebook.js
+++ src/components/notebook/Notebook.js
@@ -1,60 +1,16 @@
-import React, { useEffect, useState } from "react";
-import "./Notebook.css";
-import "materialize-css/extras/noUiSlider/nouislider.css";
-import noUiSlider from "materialize-css/extras/noUiSlider/nouislider";
+import React, { useState } from "react";
+import styles from "./notebook.module.css";
 import { useHistory } from "react-router-dom";
 import Carousel from "../carousel/Carousel";
+import { Row, Col } from "react-materialize";
+import Slider from "@material-ui/core/Slider";
 
 function Notebook() {
   const [note, setNote] = useState("I will note it later");
-  const [range, setRange] = useState({
-    start: 0,
-    end: 0,
-  });
+  const [range, setRange] = useState([0, 100]);
 
   // returns a history object, which allows to force redirection (temp)
   const history = useHistory();
-
-  useEffect(() => {
-    const slider = initializeRange();
-    bindEventListenerSlider(slider);
-  }, []);
-
-  const initializeRange = () => {
-    const slider = document.getElementById("slider");
-    noUiSlider.create(slider, {
-      start: [0, 80],
-      step: 1,
-      connect: true,
-      range: {
-        min: 0,
-        max: 100,
-      },
-      format: {
-        from: function (value) {
-          return parseInt(value);
-        },
-        to: function (value) {
-          return parseInt(value);
-        },
-      },
-    });
-
-    // referring to the documentation, this is one of the possible ways to bind an event listener
-    return slider;
-  };
-
-  const bindEventListenerSlider = (slider) => {
-    slider.noUiSlider.on("update", (values) => {
-      setRange((prevState) => {
-        const rangeObject = {
-          start: values[0],
-          end: values[1],
-        };
-        return { ...prevState, ...rangeObject };
-      });
-    });
-  };
 
   const handleOnChangeTextArea = (event) => {
     if (!event.target.value.trim()) {
@@ -69,21 +25,29 @@ function Notebook() {
     return null;
   };
 
+  const handleRangeChange = (_, newValue) => {
+    setRange(newValue);
+  };
+
   return (
-    <div id="notebook" className="notebook__wrapper">
+    <div id="notebook" className={styles.notebook__wrapper}>
       <Carousel />
-      <div className="slider__margin">
-        <div className="row">
-          <div className="col s12 m10 offset-m1 l8 offset-l2">
-            <p className="note__hint">Range of pages</p>
-            <div className="input-field">
-              <div id="slider"></div>
-            </div>
-          </div>
-        </div>
+      <div className={styles.slider__margin}>
+        <Row>
+          <Col s={12} m={10} l={8} offset="m1 l2">
+            <p className={styles.note__hint}>Range of pages</p>
+            <Slider
+              value={range}
+              onChange={handleRangeChange}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              max={729}
+            />
+          </Col>
+        </Row>
       </div>
-      <div className="row">
-        <div className="col s12 m10 offset-m1 l8 offset-l2">
+      <Row>
+        <Col s={12} m={10} l={8} offset="m1 l2">
           <div className="input-field">
             <i className="material-icons prefix">format_align_left</i>
             <textarea
@@ -94,9 +58,9 @@ function Notebook() {
             ></textarea>
             <label htmlFor="textarea2">Your thoughts</label>
           </div>
-        </div>
-      </div>
-      <div className="row center-align">
+        </Col>
+      </Row>
+      <Row className="center-align">
         <a
           href="#!"
           className="waves-effect waves-light btn"
@@ -104,7 +68,7 @@ function Notebook() {
         >
           <i className="material-icons left">cloud</i>Save
         </a>
-      </div>
+      </Row>
     </div>
   );
 }
